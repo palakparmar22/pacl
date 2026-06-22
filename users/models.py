d90db37343c40bcc7da6c63de04ae248a9e46254
palak-home <parmarpalakk@gmail.com>
2026-06-22 20:08:25 +0530

    Update db

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ class Users(models.Model):
     sr_no = models.IntegerField()
     name = models.CharField(max_length=100)
     certificate_no = models.CharField(max_length=20)
-    password = models.CharField(max_length=50)
     is_editable = models.IntegerField(null=True, blank=True)
     is_completed = models.IntegerField(null=True, blank=True)
     
@@ -17,7 +16,6 @@ class Users_M(models.Model):
     sr_no = models.IntegerField()
     name = models.CharField(max_length=100)
     certificate_no = models.CharField(max_length=20)
-    password = models.CharField(max_length=50)
     is_editable = models.IntegerField(null=True, blank=True)
     is_completed = models.IntegerField(null=True, blank=True)
     
